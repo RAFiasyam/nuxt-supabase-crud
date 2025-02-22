3d4870092d08ef2f5577cb40f018de333ccab162
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-22 14:29:20 +0700

    testing supabase

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/Xamfix/Documents/code-here/nuxt-supabase-crud/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient(
+  'https://qhzindbhulgpuvgfglvo.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoemluZGJodWxncHV2Z2ZnbHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3NzU0MjMsImV4cCI6MjA1NTM1MTQyM30.vz-gfjO7PKLRYe6Jm8vsl9_j2KME2av7T7DCeEsTXQE'
+)
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase
+    .from('instruments')
+    .select()
+  instruments.value = data
+}
+
+onMounted(() => {
+  getInstruments()
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-blue-950 h-screen text-white">
+    <ul>
+      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+    </ul>
   </div>
 </template>
